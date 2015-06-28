f3ebdc08fe4546a4f7112474a92f7c7067846c12
Benny Neugebauer <bn@bennyn.de>
2015-06-29 00:14:07 +0200

    Added Markdown4jProcessor

--- src/main/java/com/welovecoding/webapp/markdown/HelloWorld.java
+++ src/main/java/com/welovecoding/webapp/markdown/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.welovecoding.webapp.markdown;
 
+import org.markdown4j.Markdown4jProcessor;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+  public static void main(String[] args) throws Exception {
+    String html = new Markdown4jProcessor().process("This is a **bold** text");
+    System.out.println(html);
+  }
+
 }
